8ce6d6fb5c8cce990ae0f96eed8c4319dde41e4c
João Eduardo Medeiros <joaoribeiromedeiros@gmail.com>
2017-10-30 22:56:43 -0300

    Finish inital implementation of widget

--- widgets/display_fila/src/js/main.js
+++ widgets/display_fila/src/js/main.js
@@ -6,9 +6,61 @@
  * Licensed under the MIT license.
  */
 
-/* globals DisplayFila */
+/* exported DisplayFila */
 
-window.onload = function () {
+let nsgi_server;
+
+var DisplayFila = (function () {
+
     "use strict";
-    new DisplayFila();
-};
+
+    nsgi_server = MashupPlatform.prefs.get('ngsi_server');
+
+    // =========================================================================
+    // CLASS DEFINITION
+    // =========================================================================
+
+    var DisplayFila = function DisplayFila() {
+        MashupPlatform.prefs.registerCallback(function (new_preferences) {
+            nsgi_server = new_preferences.nsgi_server;
+        }.bind(this));
+    };
+
+    // =========================================================================
+    // PRIVATE MEMBERS
+    // =========================================================================
+
+    /* test-code */
+    DisplayFila.prototype = {
+    };
+    /* end-test-code */
+
+    var options = {
+        request_headers: {},
+        use_user_fiware_token: false
+    }
+
+	function getEntities() {
+		MashupPlatform.http.makeRequest('http://35.199.67.193:1026/v1/contextEntities/clinica_fila_unica/attributes/posicaoAtual', {
+		    method: "GET",
+		    contentType: "application/json",
+		    onSuccess: function (response) {
+		    	let responseJSON = JSON.parse(response.response);
+		       	let posicaoAtual = responseJSON.attributes[0].value;
+		       	console.log(posicaoAtual);
+		       	document.getElementById("posicao-fila").innerHTML = posicaoAtual;
+		    },
+		    onFailure: function (response) {
+		        // Something went wrong
+		    },
+		    onComplete: function () {
+		    	setTimeout(getEntities, 100);
+		    }
+		});
+	}
+
+	getEntities();
+
+    return DisplayFila;
+
+})();
